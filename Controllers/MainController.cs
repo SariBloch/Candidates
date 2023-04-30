10c25c5407251902e78882961c77ef28f117e5d1
SariBloch <96528768+SariBloch@users.noreply.github.com>
2023-04-30 08:49:01 +0300

    C# ServerSide

---
+++ Controllers/MainController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace EmployeesApi.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class MainController : ApiController
+    {
+   
+
+
+        [HttpGet]
+        public IHttpActionResult GetData()
+        {
+            try
+            {
+                string data = "{\"Candidates\":[{\"id\":\"1\",\"name\":\"Moshe\",\"beginYear\":\"2020\",\"lastUpdateDate\":\"2022-11-03\",\"languages\":[1,3]},{\"id\":\"20\",\"name\":\"Avi\",\"beginYear\":\"2020\",\"lastUpdateDate\":\"2021-09-27\",\"languages\":[3]},{\"id\":\"300\",\"name\":\"Gila\",\"beginYear\":\"\",\"lastUpdateDate\":\"2023-01-01\",\"languages\":[2,4,7]},{\"id\":\"300\",\"name\":\"Rina\",\"beginYear\":\"2019\",\"lastUpdateDate\":\"2021-05-17\",\"languages\":[2,3,6]}],\"Languages\":[{\"id\":1,\"name\":\"Java\"},{\"id\":2,\"name\":\".NET\"},{\"id\":3,\"name\":\"NodeJS\"},{\"id\":4,\"name\":\"AdvancedVanillaJS\"},{\"id\":5,\"name\":\"React\"},{\"id\":6,\"name\":\"Angular\"},{\"id\":7,\"name\":\"Kotlin\"},{\"id\":8,\"name\":\"Dart\"},{\"id\":9,\"name\":\"Flutter\"}]}";
+                Root root = JsonSerializer.Deserialize<Root>(data);
+                return Ok(data);
+                //return root;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+
+
+}
---
+++ Models/Model.cs
@@ -0,0 +1,23 @@
+﻿// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+using System.Collections.Generic;
+
+public class Candidate
+{
+    public string id { get; set; }
+    public string name { get; set; }
+    public string beginYear { get; set; }
+    public string lastUpdateDate { get; set; }
+    public List<int> languages { get; set; }
+}
+
+public class Language
+{
+    public int id { get; set; }
+    public string name { get; set; }
+}
+
+public class Root
+{
+    public List<Candidate> Candidates { get; set; }
+    public List<Language> Languages { get; set; }
+}
